aaf22255241266315f761354137d682b24b72235
Perry Obara <perryobara@gmail.com>
2019-12-10 11:36:35 +0300

    added pictures

---
+++ js/kabla.js
@@ -0,0 +1,12 @@
+
+var dueDate=new Date(2019,11,12);
+var  todayDate=new Date();
+
+var dateDiff=dueDate.valueOf()-todayDate.valueOf();
+var dateDiffDays=dateDiff/1000/60/60/24;
+var roundDateDiffDays=Math.round(dateDiffDays);
+console.log("the days remaining are"+""+ roundDateDiffDays);
+
+var para=document.createElement("p");
+var element=document.getElementById("dayss").innerHTML=roundDateDiffDays+" days to go";
+var node=document.createTextNode(roundDateDiffDays);
